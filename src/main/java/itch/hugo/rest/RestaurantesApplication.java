384c6c5cbaa0ade9c96e12870166f38513ccb888
Hugo81189 <hugo81189@gmail.com>
2025-12-01 19:49:04 -0600

    Adaptando properties a variables de entorno 2

--- src/main/java/itch/hugo/rest/RestaurantesApplication.java
+++ src/main/java/itch/hugo/rest/RestaurantesApplication.java
@@ -3,6 +3,10 @@ package itch.hugo.rest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableFeignClients
@@ -10,6 +14,24 @@ public class RestaurantesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestaurantesApplication.class, args);
+	}
+	
+	@Configuration
+	public class GlobalCorsConfig {
+
+	    @Bean
+	    public WebMvcConfigurer corsConfigurer() {
+	        return new WebMvcConfigurer() {
+	            @Override
+	            public void addCorsMappings(CorsRegistry registry) {
+	                registry.addMapping("/**")
+	                        .allowedOrigins("http://localhost:3000", "http://localhost:5173") // Add your frontend URLs here
+	                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+	                        .allowedHeaders("*")
+	                        .allowCredentials(true);
+	            }
+	        };
+	    }
 	}
 
 }
